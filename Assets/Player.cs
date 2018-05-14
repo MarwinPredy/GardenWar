17af2795463c3135420cf7cd2db54a37fe6403d3
MarwinP <predymarwin@gmail.com>
2018-05-14 22:38:54 +0100

    v.2

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public float speed;
+    public float sensibilite;
+    public float saut;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,23 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        // Deplacement avec les bidings de Unity dans les inputs
+
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed);
+
+        // Déplacement en fonction de la souris 
+
+        transform.Rotate(0, Input.GetAxis("Mouse X")*sensibilite, 0);
+        
+
+        // Saut
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody> ().velocity = new Vector3(0, saut, 0);
+        }
+
+    }
 }
